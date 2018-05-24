ec71b79ff222eacea7db06629c7c145e01db281c
LordKino <r.g.filippov@gmail.com>
2018-05-24 17:51:37 +0300

    Update Oci8Statement.php

--- oci8/Oci8Statement.php
+++ oci8/Oci8Statement.php
@@ -8,16 +8,32 @@
 
 namespace lordkino\oci8pdo\oci8;
 
+use ReflectionClass;
 
-class Oci8Statement extends \yajra\Pdo\Oci8\Statement {
+
+class Oci8Statement extends \Yajra\Pdo\Oci8\Statement {
 
     /**
      * Closes the cursor, enabling the statement to be executed again.
      *
      * @return bool
      */
+    private $_sth;
+    
+    private $args;
+    
+    public function __construct(...$args) {
+        parent::__construct(...$args);
+        $this->args = $args;
+    }
+    
     public function closeCursor()
     {
+        $prop = (new ReflectionClass('\Yajra\Pdo\Oci8\Statement'))->getProperty('sth');
+        $prop->setAccessible(true);
+        
+        $this->_sth = $prop->getValue(new \Yajra\Pdo\Oci8\Statement(...$this->args));
+        
         //Because we use OCI8 functions, we don't need this.
         return oci_free_statement($this->_sth);
     }
